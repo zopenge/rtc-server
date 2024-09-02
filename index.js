c0e1c571dc40a95c2de718df0be095dd8c52d17b
wujunhua <wujunhua@uino.com>
2024-09-02 22:45:32 +0800

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { WebSocketServer } = require('ws');
 console.log('test1111111111111111');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 console.log('2 - -------------- test1111111111111111');
 
