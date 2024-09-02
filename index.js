b9335e1673e1bf637584e6dcd3cdc892e728fe9a
wujunhua <wujunhua@uinnova.com>
2024-09-02 17:27:16 +0800

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 
+// test
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
